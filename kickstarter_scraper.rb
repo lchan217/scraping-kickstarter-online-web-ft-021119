8d2bfeee7106fe79d5e11d07b1d6bfdeea0e5573
Lizzie Chan <lchan217@gmail.com>
2019-03-08 20:31:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 def create_project_hash
   html = open('https://www.goodreads.com/choiceawards/best-of-the-best-2018')
     doc = Nokogiri::HTML(html)
-    
+    binding.pry
   
  
 end
